62c0d6be2ce503bf8edd402204d2f4e3cb6eed6a
devangel112 <dev.angel.perez@gmail.com>
2022-03-08 09:11:27 -0600

    Completados los métodos: Insertar, Modificar, Desactivar y Listar en ActoresWS

--- src/main/java/com/netflix/itlp/controllers/ActoresWS.java
+++ src/main/java/com/netflix/itlp/controllers/ActoresWS.java
@@ -1,23 +1,24 @@
 package com.netflix.itlp.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
+import com.netflix.itlp.dao.ActoresJDBC;
 import com.netflix.itlp.models.Actores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/actor")
@@ -25,11 +26,15 @@ import org.springframework.web.bind.annotation.RestController;
         RequestMethod.PATCH })
 public class ActoresWS {
 
+    @Autowired
+    ActoresJDBC repoActores;
+
     // localhost:8181/actores/
     @PostMapping("/actores")
     public ResponseEntity<?> insertarActor(@RequestBody Actores nuevoActor) {
         try {
-            return new ResponseEntity<>(HttpStatus.CREATED);
+            int id = repoActores.insertar(nuevoActor);
+            return new ResponseEntity<>(id, HttpStatus.CREATED);
         } catch (DataAccessException e) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
@@ -40,6 +45,7 @@ public class ActoresWS {
     public ResponseEntity<?> modificarActor(@PathVariable("actor-id") int actorId,
             @RequestBody Actores actor) {
         try {
+            repoActores.modificar(actorId, actor);
             return new ResponseEntity<>(HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
@@ -49,7 +55,22 @@ public class ActoresWS {
     // localhost:8181/actores/{actor-id}
     @DeleteMapping("/{actor-id}")
     public ResponseEntity<?> desactivarActor(@PathVariable("actor-id") int actorId) {
-        return new ResponseEntity<>(HttpStatus.OK);
+        try {
+            repoActores.desactivar(actorId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> listarActores() {
+        try {
+            List<Actores> resultado = repoActores.listar();
+            return new ResponseEntity<List<Actores>>(resultado, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
 
 }
